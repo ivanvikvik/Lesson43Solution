be588c051fa844409522412672a8f8c19f5639c6
ivanvikvik <ivanvikvik@gmail.com>
2026-02-23 17:54:35 +0300

    Add project files.

---
+++ Test01Project/main.cpp
@@ -0,0 +1,32 @@
+#include "util.h"
+
+int main() {
+	int array[DEFAULT_SIZE];
+
+	int length;
+	int a = 0, b = 20;
+
+	do {
+		cout << "Input size of array: ";
+		cin >> length;
+	} while (length <= 0);
+
+	init_random(array, length, a, b);
+
+	print("Result array:\n");
+	print(convert(array, length) + "\n");
+
+	int value = 17;
+
+	bool result = search_linear(array, length, value);
+
+	print(result ? "Yes" : "No");
+
+	print("Index of first searching element - " 
+		+ to_string(get_first_element(array, length, value)) + "\n");
+
+	print("Index of last searching element - " 
+		+ to_string(get_last_element(array, length, value)) + "\n");
+
+	return 0;
+}
